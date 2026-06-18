a8f7a34cd58134b57fe909554c3d6767d7c1a89e
chrxduff-cmyk <chrxduff@gmail.com>
2026-06-17 22:31:51 -0300

    Update Vite config with recommended build and dev settings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,35 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    port: 3000,
+    open: true,
+    strictPort: false
+  },
+
   build: {
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000,
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          'utils': ['axios']
+        }
+      }
+    }
+  },
+
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 })
